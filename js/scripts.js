f29fb00bd5ce2176991920ed436ace410bde929c
ADMIN\Admin <nguyenphilongkma@gmail.com>
2020-12-09 14:17:41 +0700

    bvn buoi 7, 8

--- js/scripts.js
+++ js/scripts.js
@@ -17,3 +17,18 @@ openWeb = () => {
       window.open("https://meta.vn");
   }
 };
+
+tongArr = () => {
+  let myArray = [1, 2, 3, 4, 6, 8, 8, 7];
+  let sum = 0;
+  myArray.forEach((x) => (sum += x));
+  let result = String(sum);
+  console.log(result);
+  document.getElementById("sumArray").innerHTML = "Tong day so: " + result;
+};
+
+changeFormat = () => {
+document.getElementsByTagName("h1")[0].innerHTML = "HELLO2";
+document.getElementsByTagName("h1")[0].outerHTML = "<h2> HELLO2 </h2>";
+document.getElementsByTagName("h2")[1].style.color = "red";
+};
